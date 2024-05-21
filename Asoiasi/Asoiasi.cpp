e189f1678bec02598cdd521563855b3a4bf0fb05
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 13:32:58 +0700

    mendeklarasikan semua methode diluar class

--- Asoiasi/Asoiasi.cpp
+++ Asoiasi/Asoiasi.cpp
@@ -40,9 +40,41 @@ public:
 		cout << "dokter \"" << nama << "\" tidak ada\n";
 	}
 
-	void tambahPasien();
+	void tambahPasien(pasien*);
 	void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter)
+{
+	daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter()
+{
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter)
+	{
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien)
+{
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien()
+{
+	cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien)
+	{
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
 
 int main()
 {
