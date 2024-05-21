18c608997cf4147f138722f84121ceac0c81b1ae
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 11:26:51 +0700

    mendeklarasikan methode diluar class

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -20,4 +20,20 @@ public:
 	void tambahAnak(anak*);
 	void cetakAnak();
 };
+
+void Ibu::tambahAnak(anak*pAnak)
+{
+	daftar_anak.push_bsck(pAnak);
+}
+
+void Ibu::cetakAnak()
+{
+	cout << "Daftar Anak dari Ibu\"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak)
+	{
+		cout << a->nama << "\n";
+		
+	}
+	cout << endl;
+}
 #endif
