29fdec03d36877f4a955898d9bafbbc9eb7c2670
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 11:06:06 +0700

    membuat program utama

--- composition/composition.cpp
+++ composition/composition.cpp
@@ -1,8 +1,15 @@
 
 #include <iostream>
+#include<string>
+using namespace std;
+
+#include "Jantung.h"
+#include "Manusia.h"
 
 int main()
 {
-   
+	Manusia* varManusia = new Manusia("jono");
+	delete varManusia;
+	return 0;  
 }
 
