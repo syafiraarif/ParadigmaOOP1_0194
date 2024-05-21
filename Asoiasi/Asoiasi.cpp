a5975876c9623056727ae6f6230df407db583876
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 13:15:45 +0700

    membuat class pasien

---
+++ Asoiasi/Asoiasi.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien
+{				//asosiasi
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama)
+	{
+		cout << "pasien\"" << nama << "\" ada\n ";
+	}
+
+	~pasien()
+	{
+		cout << "pasien\"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
+
+int main()
+{
+   
+}
+
