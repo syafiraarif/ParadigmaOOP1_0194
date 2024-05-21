d504788055859aa1360d5ce3c878140e7bc6b2e0
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 10:38:54 +0700

    membuat class untuk objek mahasiswa

--- Pewaris/Pewaris.cpp
+++ Pewaris/Pewaris.cpp
@@ -4,6 +4,26 @@
 using namespace std;
 //memberinitlist
 
+class Mahasiswa
+{
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	Mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai)
+	{
+		//membuat definisi
+	}
+
+	~Mahasiswa()
+	{
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
 
 int main()
 {
