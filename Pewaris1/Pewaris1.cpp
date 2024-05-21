8ecca3407b293f351cf55528de2eedab5a3f21e6
syairaarif <putrisyafira4444@gmail.com>
2024-05-21 13:39:40 +0700

    membuat class untuk objek orang

--- Pewaris1/Pewaris1.cpp
+++ Pewaris1/Pewaris1.cpp
@@ -1,9 +1,31 @@
 
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class orang
+{
+public:
+    string nama;
+    orang(string pNama) :nama(pNama)
+    {
+        cout << "orang dibuat\n" << endl;
+    }
+
+    ~orang()
+    {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
